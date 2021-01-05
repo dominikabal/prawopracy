9afe1390c24318833d5e9531cb943167982bf9db
dominikabal <74771198+dominikabal@users.noreply.github.com>
2021-01-05 17:18:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,134 @@
+$("#quiz1").dlxQuiz({
+    quizData: {
+        questions: [
+            {
+                q:
+                    'Kodeks pracy określa prawa i obowiązki:',
+                a: "pracowników i pracodawców",
+                options: [
+                    "pracowników",
+                    "pracodawców",
+                    "pracowników i pracodawców"
+                ]
+            },
+            {
+
+                q:
+                'Pracownikiem jest osoba zatrudniona na podstawie:',
+            a: "umowy o pracę, powołania, wyboru, mianowania lub spółdzielczej umowy o pracę",
+            options: [
+                "umowy o pracę, powołania, wyboru, mianowania lub spółdzielczej umowy o pracę",
+                "umowy o pracę, wyboru, mianowania lub spółdzielczej umowy o pracę",
+                "umowy o pracę, powołania, wyboru lub spółdzielczej umowy o pracę"]
+            },
+{
+q: 
+'Zgodnie z kodeksem pracy, pracodawcą jest:',
+a:"jednostka organizacyjna, choćby nie posiadała osobowości prawnej, a także osoba fizyczna, jeżeli zatrudniają one pracowników",
+options:[
+    "jednostka organizacyjna, choćby nie posiadała osobowości prawnej, a także osoba fizyczna, jeżeli zatrudniają one pracowników",
+     "jednostka organizacyjna nieposiadająca osobowości prawnej, a także osoba fizyczna, jeżeli zatrudniają one pracowników",
+     "jednostka organizacyjna posiadająca osobowość prawną, a także osoba fizyczna, jeżeli zatrudniają one pracowników"]
+
+                                               
+},       
+
+{
+    q:'Podmiotem dokonującym czynności z zakresu prawa pracy jest:',
+    a:"osoba lub organ zarządzający tą jednostką albo inna wyznaczona do tego osoba",
+    options:["osoba lub organ zarządzający tą jednostką albo inna wyznaczona do tego osoba","organ zarządzający tą jednostką albo inna wyznaczona do tego osoba","osoba lub organ zarządzający tą jednostką ",]
+    },
+
+
+    {
+        q:'Jeżeli stosunek pracy określonej kategorii pracowników regulują przepisy szczególne',
+        a:"przepisy kodeksu stosuje się w zakresie nieuregulowanym tymi przepisami",
+        options:["przepisy kodeksu stosuje się w zakresie nieuregulowanym tymi przepisami","przepisy kodeksu stosuje się w zakresie uregulowanym tymi przepisami","przepisy innych kodeksów stosuje się w zakresie nieuregulowanym tymi przepisami"]
+        },
+
+        {
+            q:'Nie można czynić ze swego prawa użytku, który byłby sprzeczny:',
+            a:"ze społeczno-gospodarczym przeznaczeniem tego prawa lub zasadami współżycia społecznego - takie działanie lub zaniechanie uprawnionego nie jest uważane za wykonywanie prawa i nie korzysta z ochrony",
+            options:["ze społeczno-gospodarczym przeznaczeniem tego prawa lub zasadami współżycia społecznego - takie działanie lub zaniechanie uprawnionego nie jest uważane za wykonywanie prawa i nie korzysta z ochrony","ze społeczno-gospodarczym przeznaczeniem tego prawa lub zasadami współżycia społecznego oraz interesem społecznym - takie działanie lub zaniechanie uprawnionego nie jest uważane za wykonywanie prawa i nie korzysta z ochrony","ze społeczno-gospodarczym przeznaczeniem tego prawa lub zasadami współżycia społecznego - takie działanie lub zaniechanie uprawnionego nie jest uważane za wykonywanie prawa i korzysta z ochrony"]
+            },     
+            
+            {
+                q:'Ilekroć w Kodeksie pracy jest mowa o prawie pracy, rozumie się przez to:',
+                a:"przepisy Kodeksu pracy oraz przepisy innych ustaw i aktów wykonawczych, określające prawa i obowiązki pracowników i pracodawców, a także postanowienia układów zbiorowych pracy i innych opartych na ustawie porozumień zbiorowych, regulaminów i statutów określających prawa i obowiązki stron stosunku pracy",
+                options:["przepisy Kodeksu pracy oraz przepisy innych ustaw i aktów wykonawczych, określające prawa i obowiązki pracowników i pracodawców, a także postanowienia układów zbiorowych pracy i innych opartych na ustawie porozumień zbiorowych, regulaminów i statutów określających prawa i obowiązki stron stosunku pracy","przepisy Kodeksu pracy oraz przepisy innych ustaw i aktów wykonawczych, określające prawa i obowiązki pracowników, a także postanowienia układów zbiorowych pracy i innych opartych na ustawie porozumień zbiorowych, regulaminów i statutów określających prawa i obowiązki stron stosunku pracy","przepisy Kodeksu pracy oraz przepisy innych ustaw i aktów wykonawczych, określające prawa i obowiązki pracowników i pracodawców, a także postanowienia układów zbiorowych pracy, regulaminów i statutów określających prawa i obowiązki stron stosunku pracy"]
+                },
+
+                {
+                    q:'Porozumienie o zawieszeniu stosowania przepisów prawa pracy zawiera:',
+                    a:"pracodawca i reprezentująca pracowników organizacja związkowa, a jeżeli pracodawca nie jest objęty działaniem takiej organizacji, porozumienie zawiera pracodawca i przedstawicielstwo pracowników wyłonione w trybie przyjętym u tego pracodawcy",
+                    options:["pracodawca i reprezentująca pracowników organizacja związkowa, a jeżeli pracodawca nie jest objęty działaniem takiej organizacji, porozumienie zawiera pracodawca i przedstawicielstwo pracowników wyłonione w trybie przyjętym u tego pracodawcy","pracodawca i reprezentująca pracowników organizacja związkowa, a jeżeli pracodawca nie jest objęty działaniem takiej organizacji, porozumienie zawiera pracodawca i pełnomocnik pracowników wyłoniony w trybie przyjętym u tego pracodawcy","pracodawca, a jeżeli pracodawca nie jest objęty działaniem takiej organizacji, porozumienie zawiera pracodawca i przedstawicielstwo pracowników wyłonione w trybie przyjętym u tego pracodawcy"]
+                    },
+                    {
+                        q:'Zawieszenie stosowania przepisów prawa pracy nie może trwać dłużej niż:',
+                        a:"przez okres 3 lat",
+                        options:["przez okres 3 lat","przez okres 2 lat","przez okres 5 lat"]
+                        },
+
+                        {
+                            q:'Porozumienie o zawieszeniu stosowania przepisów prawa pracy przekazuje:',
+                            a:"pracodawca właściwemu okręgowemu inspektorowi pracy",
+                            options:["pracodawca właściwemu okręgowemu inspektorowi pracy","pracodawca lub pełnomocnik pracodawcy właściwemu okręgowemu inspektorowi pracy","pracodawca, osoba lub organ zarządzający tą jednostką albo inna wyznaczona do tego osoba właściwemu okręgowemu inspektorowi pracy"]
+                            },
+                            {
+                                q:'Zgodnie z zasadami prawa pracy:',
+                                a:"każdy ma prawo do swobodnie wybranej pracy. Nikomu, z wyjątkiem przypadków określonych w ustawie, nie można zabronić wykonywania zawodu",
+                                options:["każdy ma prawo do swobodnie wybranej pracy. Nikomu, z wyjątkiem przypadków określonych w ustawie, nie można zabronić wykonywania zawodu","każdy ma prawo do swobodnie wybranej pracy. Nikomu nie można zabronić wykonywania zawodu","każdy ma prawo do swobodnie wybranej pracy. Nikomu, z wyjątkiem przypadków określonych w ustawie, można zabronić wykonywania zawodu"]
+                                },
+
+                                {
+                                    q:'Minimalną wysokość wynagrodzenia za pracę określa:',
+                                    a:"państwo",
+                                    options:["państwo","Rada Ministrów","Ministerstwo Finansów"]
+                                    },
+
+                                    {
+                                        q:'Zgodnie z zasadą swobodnego nawiązywania stosunku pracy:',
+                                        a:"nawiązanie stosunku pracy oraz ustalenie warunków pracy i płacy, bez względu na podstawę prawną tego stosunku, wymaga zgodnego oświadczenia woli pracodawcy i pracownika",
+                                        options:["nawiązanie stosunku pracy oraz ustalenie warunków pracy i płacy, bez względu na podstawę prawną tego stosunku, wymaga zgodnego oświadczenia woli pracodawcy i pracownika","nawiązanie stosunku pracy, bez względu na podstawę prawną tego stosunku, wymaga zgodnego oświadczenia woli pracodawcy i pracownika", "nawiązanie stosunku pracy oraz ustalenie warunków pracy i płacy, ze względu na podstawę prawną tego stosunku, wymaga zgodnego oświadczenia woli pracodawcy i pracownika"]
+                                        
+                                        },
+                                        
+                                        {
+                                            q:'Według zasady równego traktowania pracowników:',
+                                            a:"pracownicy mają równe prawa z tytułu jednakowego wypełniania takich samych obowiązków; dotyczy to w szczególności równego traktowania mężczyzn i kobiet w zatrudnieniu",
+                                            options:["pracownicy mają równe prawa z tytułu jednakowego wypełniania takich samych obowiązków; dotyczy to w szczególności równego traktowania mężczyzn i kobiet w zatrudnieniu","pracownicy i pracodawcy mają równe prawa z tytułu jednakowego wypełniania takich samych obowiązków; dotyczy to w szczególności równego traktowania mężczyzn i kobiet w wykonywaniu obowiązków","pracownicy mają równe prawa z tytułu jednakowego wypełniania takich samych obowiązków; dotyczy to w szczególności równego traktowania pracowników tej samej płci w zatrudnieniu"]
+                                            }, 
+                                    
+{
+q:'Zgodnie z zakazem dyskryminacji w zatrudnieniu, dyskryminacja:',
+a:"bezpośrednia lub pośrednia, w szczególności ze względu na płeć, wiek, niepełnosprawność, rasę, religię, narodowość, przekonania polityczne, przynależność związkową, pochodzenie etniczne, wyznanie, orientację seksualną, zatrudnienie na czas określony lub nieokreślony, zatrudnienie w pełnym lub w niepełnym wymiarze czasu pracy - jest niedopuszczalna",
+options:["bezpośrednia lub pośrednia, w szczególności ze względu na płeć, wiek, niepełnosprawność, rasę, religię, narodowość, przekonania polityczne, przynależność związkową, pochodzenie etniczne, wyznanie, orientację seksualną, zatrudnienie na czas określony lub nieokreślony, zatrudnienie w pełnym lub w niepełnym wymiarze czasu pracy - jest dopuszczalna","bezpośrednia lub pośrednia, w szczególności ze względu na płeć, wiek, niepełnosprawność, rasę, religię, narodowość, przekonania polityczne, przynależność związkową, pochodzenie etniczne, wyznanie, orientację seksualną, zatrudnienie na czas określony lub nieokreślony, zatrudnienie w pełnym lub w niepełnym wymiarze czasu pracy - jest niedopuszczalna","bezpośrednia lub pośrednia, w szczególności ze względu na płeć, wiek, niepełnosprawność, rasę, religię, status społeczny, narodowość, przekonania polityczne, przynależność związkową, pochodzenie etniczne, wyznanie, orientację seksualną, zatrudnienie na czas określony lub nieokreślony, zatrudnienie w pełnym lub w niepełnym wymiarze czasu pracy - jest niedopuszczalna"]
+},
+
+{
+    q:'Pracownik ma prawo do godziwego wynagrodzenia za pracę. Warunki realizacji tego prawa określają:',
+    a:"przepisy prawa pracy oraz polityka państwa w dziedzinie płac, w szczególności poprzez ustalanie minimalnego wynagrodzenia za pracę",
+    options:["przepisy prawa pracy oraz polityka państwa w dziedzinie płac, w szczególności poprzez ustalanie minimalnego wynagrodzenia za pracę","przepisy prawa pracy oraz państwo, w szczególności poprzez ustalanie minimalnego wynagrodzenia za pracę","przepisy prawa pracy oraz polityka państwa w dziedzinie płac, w szczególności poprzez ustalanie kwartalnego, minimalnego wynagrodzenia za pracę"]
+    },
+
+    {
+        q:'Zgodnie z art. 14 Kodeksu pracy, prawo do wypoczynku zapewniają:',
+        a:"przepisy o czasie pracy, dniach wolnych od pracy oraz o urlopach wypoczynkowych",
+        options:["pracodawca oraz przepisy o czasie pracy, dniach wolnych od pracy oraz o urlopach wypoczynkowych","przepisy o czasie pracy, dniach wolnych od pracy oraz o urlopach wypoczynkowych","Konstytucja RP oraz przepisy o czasie pracy, dniach wolnych od pracy oraz o urlopach wypoczynkowych"]
+        },
+
+        {
+            q:'Pracodawca jest obowiązany:',
+            a:"zapewnić pracownikom bezpieczne i higieniczne warunki pracy",
+            options:["zapewnić pracownikom bezpieczne i higieniczne warunki pracy","zapewnić pracownikom bezpieczne i higieniczne miejsce pracy","zapewnić pracownikom bezpieczne i higieniczne wykonywanie pracy"]
+            },
+            {
+                q:'Pracodawca zaspokaja potrzeby pracowników:',
+                a:"stosownie do możliwości i warunków, zaspokaja bytowe, socjalne i kulturalne potrzeby pracowników",
+                options:["stosownie do możliwości zaspokaja bytowe, socjalne i kulturalne potrzeby pracowników","stosownie do możliwości i warunków, zaspokaja bytowe, społeczne i kulturalne potrzeby pracowników","stosownie do możliwości i warunków, zaspokaja bytowe, socjalne i kulturalne potrzeby pracowników"]
+                },                 
+        
+
+        ]
+    }
+});
